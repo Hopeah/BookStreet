b797c90a13401be410721d656c519845959769f2
Irina <irina@theluky.com>
2022-08-25 12:30:07 -0400

    Add a working about page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const frontRoutes = require ('./routes/front')
+const contactRoutes = require ('./routes/contact')
 
 require('dotenv').config({path: './config/.env'})
 
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use('/', frontRoutes)
+app.use('/contact', contactRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
