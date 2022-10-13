0eb990bb4035b4f28a5f22a262c8f0b094329e12
Irina <irina@theluky.com>
2022-10-13 15:29:20 -0400

    Add profile name change, profile links

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,13 +6,15 @@ const { ensureAuthenticated } = require('../config/Auth')
 
 router.get('/', ensureAuthenticated, dashboardController.getDashboard)
 
+router.put('/updateName', ensureAuthenticated, dashboardController.updateName)
+
+router.put('/updateAvatar', ensureAuthenticated, upload.single("avatar"), dashboardController.updateAvatar)
+
 router.get('/feed', dashboardController.getFeed)
 
 router.get('/literature', dashboardController.getLiterature)
 
 router.get('/lists', dashboardController.getLists)
-
-router.put('/updateAvatar', ensureAuthenticated, upload.single("avatar"), dashboardController.updateAvatar)
 
 router.post('/createDoc', ensureAuthenticated, upload.single("file"), dashboardController.createDoc)
 
